408103915c1c8c0b46f34b44240b281c5684165f
Ron Barrantes <ronbarrantes@gmail.com>
2018-02-01 21:39:40 -0800

    added practice code

---
+++ LinkedList.js
@@ -0,0 +1,39 @@
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+ListNode.prototype.print = function () {
+  let arr = []
+  let node = this
+  while (node) {
+    arr = [...arr, node.val]
+    node = node.next
+  }
+  console.log(arr)
+  return arr
+}
+
+ListNode.prototype.append = function (num) {
+  let node = this
+
+  if (node.val === undefined) {
+    node.val = num
+    return node
+  }
+
+  while (node.next) {
+    node = node.next
+  }
+
+  node.next = new ListNode(num)
+  return node
+}
+
+ListNode.prototype.mock = function (...nums) {
+  let arr = nums.length ? [...nums] : [1, 2, 3, 4, 5]
+  arr.forEach(num => this.append(num))
+  return this
+}
+
+module.exports = ListNode
---
+++ delete-node-from-linked-list.js
@@ -0,0 +1,42 @@
+const ListNode = require('./LinkedList')
+let list = new ListNode()
+list.mock()
+list.print()
+
+var deleteNode = function (node) {
+  let ll = list
+  let nextNode = new ListNode(0);
+  let carry = 0
+  let head = nextNode
+
+  while (ll) {
+    console.log(ll.val)
+    nextNode.val += ll.val;
+
+    if (ll.val === node.val && ll.next !== null) {
+      ll = ll.next
+      
+      console.log('found one |', ll.val)
+    }
+
+    console.log('hello')
+    ll = ll.next
+
+    if (carry === 0 && ll === null) {
+      return head
+    }
+
+    nextNode.next = new ListNode(carry);
+    nextNode = nextNode.next;
+  }
+  head.print()
+  list = head
+  return head
+}
+
+deleteNode(new ListNode(1))
+list.print()
+
+
+
+
