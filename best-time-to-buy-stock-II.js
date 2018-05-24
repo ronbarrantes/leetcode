85bb34b3a4915f14585d529b6907eed58d1584f1
Ron Barrantes <ronbarrantes@gmail.com>
2018-05-23 23:25:31 -0700

    doing some leetcode

---
+++ best-time-to-buy-stock-II.js
@@ -0,0 +1,21 @@
+var maxProfit = function (prices) {
+
+  let profit = 0
+  let price = 0
+
+
+  // if nextDay is better than the currDay
+  // buy
+
+  // if the nextDay is worse than the currDay
+  // sell 
+  // add the difference to the profit
+
+
+  return prices
+
+};
+
+
+let price = [7, 1, 5, 3, 6, 4]
+console.log(maxProfit(price))
