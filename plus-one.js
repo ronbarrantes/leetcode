832cf9419ae71e9a84c5093cb7ade923eb63567f
Ron Barrantes <ronbarrantes@gmail.com>
2018-05-26 23:57:03 -0700

    added two-sum.js

--- plus-one.js
+++ plus-one.js
@@ -1,6 +1,36 @@
 var plusOne = function (digits) {
+  let carry = 0
+  let i = digits.length - 1
+
+  while (i >= 0) {
+
+    if (digits[i] < 9) {
+      digits[i] += 1
+      break
+    }
+
+    // if the number is 9
+    if (digits[i] >= 9) {
+      carry = 1
+      digits[i] = 0
+    }
+
+    // if end of the array
+    if (carry > 0 && i === 0) {
+      digits.unshift(carry)
+      break
+    }
+    i--
+  }
+
   return digits
 };
 
 let nums = [1, 2, 3]
-console.log(plusOne(nums))
+console.log(plusOne(nums), 'Expect [1,2,4]')
+
+let nums2 = [1, 2, 9]
+console.log(plusOne(nums2), 'Expect [1,3,0]')
+
+let nums3 = [9, 9]
+console.log(plusOne(nums3), 'Expect [1,0,0]')
---
+++ two-sum.js
@@ -0,0 +1,8 @@
+var twoSum = function (nums, target) {
+  let pair = ['hello']
+
+
+  return pair
+};
+
+console.log(twoSum([1, 2, 3, 9], 11), 'Expect [1, 3]')
