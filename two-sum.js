a7fa21b02a95d17a17813fb0786cba714daf9888
Ron Barrantes <ronbarrantes@gmail.com>
2018-05-27 00:34:45 -0700

    done with two sum

--- two-sum.js
+++ two-sum.js
@@ -1,8 +1,19 @@
 var twoSum = function (nums, target) {
-  let pair = ['hello']
+  let hash = {}
+  let i = 0
 
+  while (i < nums.length) {
 
-  return pair
+    // if value appears on the hash
+    if (hash[nums[i]] || hash[nums[i]] === 0)
+      return [hash[nums[i]], i]
+
+    // if it doesnt appear
+    hash[target - nums[i]] = i
+    i++
+  }
 };
 
-console.log(twoSum([1, 2, 3, 9], 11), 'Expect [1, 3]')
+console.log(twoSum([3, 6, 11, 15], 9), 'Expect [0, 1]')
+
+console.log(twoSum([2, 5, 7, 15], 12), 'Expect [1, 2]')
