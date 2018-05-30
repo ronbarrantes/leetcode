8998f5288aff95eba033aed3f3de24565dd4d6e1
Ron Barrantes <ronbarrantes@gmail.com>
2018-05-30 00:11:23 -0700

    Added new files
    new file: first-unique-character-in-a-string.js
    new file: reverse-integer.js
    new file: valid-sudoku.js

---
+++ reverse-integer.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let checkVal = 1
+    let newVal = ''
+    
+    if (x<0) {
+        checkVal = -1
+        x *= -1
+    }
+    
+    //turn x to string
+    x = x.toString()
+    
+    //reverse it
+    for (let i = x.length-1; i>=0; i--){
+        newVal += x[i]
+    }
+    
+    if (newVal >= 2**31 - 1){
+        return 0
+    }
+    
+    // turn newVal back to number
+    return parseInt(newVal) * checkVal
+};
+
---
+++ valid-sudoku.js
@@ -0,0 +1,43 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function (board) {
+  // horizontal sweep
+  let valid = true
+
+  for (let i = 0; i < board.length; i++) {
+    let hash = {}
+    board[i]
+    .filter(val => val != '.')
+    .find(val => {
+      if (hash[val]){
+        valid = false
+      }
+      hash[val] = true
+    })
+  }
+
+  // vertical sweep
+
+  // square sweep
+
+
+  return valid
+};
+
+console.log(
+  isValidSudoku(
+    [
+      ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+      ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+      [".", "9", "8", ".", ".", ".", ".", "6", "."],
+      ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+      ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+      ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+      [".", "6", ".", ".", ".", ".", "2", "8", "."],
+      [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+      [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+    ]),
+  'EXPECT --> true'
+)
