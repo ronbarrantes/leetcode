5460c88ad99254dc4cfec64d02c4df2770466a64
ronbarrantes <ronbarrantes@gmail.com>
2018-07-29 22:57:41 -0700

    adding string to integer

--- string-to-integer.js
+++ string-to-integer.js
@@ -1,22 +1,59 @@
 var myAtoi = function (str) {
-  // pull out any whitespace or extra characters from the string
+  console.log ('MAIN STRING -->', str)
 
+  let newString = ''
+  let result = 0
+  let insertNumber = true
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].search(/[0-9- ]/) === -1)
+      insertNumber = false
+
+    if (str[i].search(/[0-9-\+]/) !== -1 && insertNumber)
+      newString += str[i]
+  }
+
+  if (typeof Number(newString) === 'number' && newString !== '-')
+    result = Number(newString)
 
   // check the string is within range
+  if (result > 2 ** 31 - 1)
+    return 2 ** 31 - 1
 
-  // return the string
+  if (result < -(2 ** 31))
+    return -(2 ** 31)
 
-  return str
+  // return result
+  return result
 };
 
 
 let input1 = '42' // expect 42
-console.log(myAtoi(input1), 'EXPECT -> 42')
 let input2 = '      -42' // expect -42
-console.log(myAtoi(input2), 'EXPECT -> -42')
 let input3 = '4193 with words' // expect 4193
-console.log(myAtoi(input3), 'EXPECT -> 4193')
-let input4 = 'words and 987' // expect 987
-console.log(myAtoi(input4), 'EXPECT -> 987')
 let input5 = '-91283472332' // -2147483648
-console.log(myAtoi(input5), 'EXPECT -> -2147483648')
+let input4 = 'words and 987' // expect 0
+let input6 = '.42' // -2147483648
+let input7 = '+' // 
+let input8 = '3.14'
+let input9 = '-'
+let input10 = '/'
+let input11 = '*'
+let input15 = '-00012abc29292'
+let input16 = '+1'
+
+console.log(myAtoi(input8), '| EXPECT -> 3')
+console.log(myAtoi(input1), '| EXPECT -> 42')
+console.log(myAtoi(input2), '| EXPECT -> -42')
+console.log(myAtoi(input3), '| EXPECT -> 4193')
+console.log(myAtoi(input5), '| EXPECT -> -2147483648')
+console.log(myAtoi(input15), '| EXPECT -> -12')
+console.log(myAtoi(input4), '| EXPECT -> 0')
+console.log(myAtoi(input6), '| EXPECT -> 0')
+console.log(myAtoi(input7), '| EXPECT -> 0')
+console.log(myAtoi(input9), '| EXPECT -> 0')
+console.log(myAtoi(input10), '| EXPECT -> 0')
+console.log(myAtoi(input11), '| EXPECT -> 0')
+console.log(myAtoi(input16), '| EXPECT -> 1')
+
+
