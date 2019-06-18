89bb1b0545c87bc970f5580be7a9d05eaf2c1784
ronbarrantes <ronbarrantes@gmail.com>
2019-06-17 23:49:08 -0700

    Add new two sum

---
+++ amazon/two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+
+  let store = {}
+
+  // iterate over the array
+  for (let i = 0; i < nums.length; i++){
+    // check in the store if there is a number that will add to the target
+    if (store[nums[i]] || store[nums[i]] === 0)
+      return [store[nums[i]], i]
+
+    // if it doesnt appear
+    store[target - nums[i]] = i
+  }
+}
