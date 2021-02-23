1a3b289a1aaa0ecbc1fed0e607f1fa3212bb11ed
ronbarrantes <ronbarrantes@gmail.com>
2021-02-22 23:13:27 -0800

    Implement LinkedList

---
+++ DataStructures/LinkedList.js
@@ -0,0 +1,61 @@
+class ListNode {
+	constructor(val) {
+		this.val = val || undefined
+	}
+}
+
+class LinkedList extends ListNode{
+	constructor(val, next) {
+		super(val)
+		this.next = next || null
+	}
+
+	/**
+	 *
+	 * @param vals A value or values to be passed to the ListNode
+	 */
+	append(...vals){
+		if(vals.length === 0){
+			return this
+		}
+
+		let node = this
+
+		while (node.next) {
+			node = node.next
+		}
+
+		if(this.val !== undefined){
+			node.next = new LinkedList()
+			node = node.next
+		}
+
+		vals.forEach((val, i) => {
+			node.val = val
+			if(i < vals.length - 1){
+				node.next = new LinkedList()
+				node = node.next
+			}
+		})
+
+		return this
+	}
+
+	// TODO: IMPLEMENT DELETE
+
+	/**
+	 * logs and returns the values of the linked list in array form
+	 */
+	log() {
+		let arr = []
+		let node = this
+		while (node) {
+			arr = [...arr, node.val]
+			node = node.next
+		}
+		console.log(arr)
+		return arr
+	}
+}
+
+module.exports = LinkedList
