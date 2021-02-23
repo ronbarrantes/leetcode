e6a6fccedecc3d928b5d4c02cf7fbc8796e44927
ronbarrantes <ronbarrantes@gmail.com>
2021-02-22 20:14:05 -0800

    Add is Palindrome

--- study21/isPalindrome.js
+++ study21/isPalindrome.js
@@ -3,10 +3,12 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    // cleanup string
+	if(s.length <= 1){
+		return true
+	}
+
 	s = s.toLowerCase()
-	s = s.replace(/\W/g, '')
-	console.log('THE STRING===>', s)
+	s = s.replace(/[\W_]/g, '')
 
 	for(let i = 0; i < s.length/2; i++){
 		const end = s.length - 1 - i
@@ -17,4 +19,10 @@ var isPalindrome = function(s) {
 	return true
 }
 
-console.log('Resolve true', isPalindrome('A man, a plan, a canal: Panama'))
+
+// console.log('Resolve true', isPalindrome('A man, a plan, a canal: Panama'))
+// console.log('Resolve false', isPalindrome('rat'))
+// console.log('Resolve true', isPalindrome('d'))
+console.log('Resolve true ==>', isPalindrome('ab_a'))
+console.log('Resolve true ==>', isPalindrome('ab-a'))
+
