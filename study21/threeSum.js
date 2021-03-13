b32a0ceaa7d320b4a50166164b49778e25e4e740
ronbarrantes <ronbarrantes@gmail.com>
2021-03-13 14:49:29 -0800

    Finish 3sum

--- study21/threeSum.js
+++ study21/threeSum.js
@@ -5,57 +5,39 @@
 
 const assert = require('assert').strict
 
-const isNotDup = (arr, res) => {
-	const length = arr.length
-	if(length === 0) return true
-
-	const a = arr[length-1][0]
-	const b = arr[length-1][1]
-	const c = arr[length-1][2]
-
-	if(a === res[0] && b === res[1] && c === res[2])
-		return false
-
-	return true
-}
-
 var threeSum = function(nums) {
-	let l = 0
-	let c = 1
-	let r = nums.length - 1
-
 	const result = []
 
-	if(nums.length <= 1)
+	if(nums.length <= 2)
 		return result
 
 	nums = nums.sort((a, b) => a - b)
 
-	while(l < r){
-		if(c === r){
-			l++
-			c = l + 1
-			continue
+	// iterate over the array
+
+	for(let i = 0; i < nums.length; i++){
+		let lo = i + 1
+		let hi = nums.length - 1
+
+		if (i === 0 || nums[i - 1] !== nums[i]){
+			while(lo < hi){
+				const sum = nums[i] + nums[lo] + nums[hi]
+				const arrNums = [nums[i], nums[lo], nums[hi]]
+
+				if(sum < 0)
+					lo++
+				else if(sum > 0)
+					hi--
+				else{
+					result.push(arrNums)
+					lo++
+					hi--
+					while(lo < hi && nums[lo] === nums[lo - 1]){
+						lo++
+					}
+				}
+			}
 		}
-
-		const sum = nums[l] + nums[c] + nums[r]
-		const arrNums = [nums[l], nums[c], nums[r]]
-		const isArrNotDup = isNotDup(result, arrNums)
-
-		if(sum === 0){
-			if(isArrNotDup)
-				result.push(arrNums)
-			l++
-			c = l + 1
-		}
-
-		if(sum > 0)
-			r--
-
-		if(sum < 0){
-			c++
-		}
-
 	}
 
 	return result
@@ -64,7 +46,13 @@ var threeSum = function(nums) {
 const a = [-1, 0, 1, 2, -1, -4]
 const b = [0, 0, 0, 0]
 const c = [-2, 0, 1, 1, 2]
+const d = [0, 0]
 
-// assert.deepStrictEqual(threeSum(a), [[-1, -1, 2], [-1, 0, 1]], `They're not equal`)
-// assert.deepStrictEqual(threeSum(b), [[0, 0, 0]], 'Need all zeros')
+threeSum(a) //?
+threeSum(b) //?
+threeSum(c) //?
+threeSum(d) //?
+
+assert.deepStrictEqual(threeSum(a), [[-1, -1, 2], [-1, 0, 1]], `They're not equal`)
+assert.deepStrictEqual(threeSum(b), [[0, 0, 0]], 'Need all zeros')
 assert.deepStrictEqual(threeSum(c), [[-2, 0, 2], [-2, 1, 1]], 'Is not right')
