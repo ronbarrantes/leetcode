ed9e9bc6ee52c44480d088807a968664f34e6c02
Ron Barrantes <ronbarrantes@gmail.com>
2025-01-29 15:01:39 -0600

    Fix can place flowers

--- study25/canPlaceFlowers.js
+++ study25/canPlaceFlowers.js
@@ -4,19 +4,27 @@
  * @return {boolean}
  */
 
+// should return a number
+const findFlowersLeft = (flowerbed, idx, count) => {
+  while (idx < flowerbed.length) {
+    if (
+      flowerbed[idx] === 0 &&
+      (!flowerbed[idx + 1] || flowerbed[idx + 1] === 0)
+    ) {
+      count--
+      idx++
+    }
+    idx++
+  }
+
+  return count
+}
+
 var canPlaceFlowers = function (flowerbed, n) {
   if (n === 0) return true
 
-  for (let i = 0; i < flowerbed.length - 1 - n * 2; i++) {
-    let flowersLeft = n
-    let p = i + 1
-
-    while (p < flowerbed.length - 1 - n * 2 && flowersLeft !== 0) {
-      if (flowerbed[p] === 0 && flowerbed[p + 1] === 0) {
-        p += 2
-        flowersLeft -= 1
-      }
-    }
+  for (let i = 0; i < flowerbed.length; i++) {
+    let flowersLeft = findFlowersLeft(flowerbed, i, n)
 
     if (flowersLeft === 0) return true
   }
@@ -28,3 +36,4 @@ console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2) === false)
 console.log(canPlaceFlowers([0], 0) === true)
 console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1) === true)
 console.log(canPlaceFlowers([0], 1) === true)
+console.log(canPlaceFlowers([1], 1) === false)
